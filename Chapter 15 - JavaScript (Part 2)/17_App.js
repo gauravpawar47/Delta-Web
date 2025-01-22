aa1bbab69bd2ae0291c432fcb38827724e586144
gauravpawar47 <gauravpawar883@gmail.com>
2025-01-22 13:48:00 +0530

    Chapter 15

---
+++ Chapter 15 - JavaScript (Part 2)/17_App.js
@@ -0,0 +1,17 @@
+let color = "red";
+
+switch(color)
+{
+    case "red":
+        console.log("STOP");
+        break;
+    case "yellow":
+        console.log("NOTICE");
+        break;
+    case "green": 
+        console.log("GO");
+        break;
+    default:
+        console.log("TRAFFIC LIGHT IS BREAKDOWN");
+        break;
+}
