a0c77391aad28dc19f422c4ff543f5694d7d04f3
gauravpawar47 <gauravpawar883@gmail.com>
2025-01-24 19:25:03 +0530

    Chapter 16

--- Chapter 16 - JavaScript (Part 3)/16_App.js
+++ Chapter 16 - JavaScript (Part 3)/16_App.js
@@ -1,9 +1,8 @@
 let str = "Gaurav Pawar";
 
 console.log("Original String : ", str);
-console.log("IndexOf Pawar : ", str.indexOf("Pawar"));
-console.log("IndexOf w : ", str.indexOf("w"));
+console.log("IndexOf Pawar   : ", str.indexOf("Pawar"));
+console.log("IndexOf w       : ", str.indexOf("w"));
 
-console.log("Includes gua : ", str.includes("gua"));
-console.log("IndexOf Gaurav : ", str.indexOf("Gaurav"));
-
+console.log("Includes gua    : ", str.includes("gua"));
+console.log("Includes Gaurav : ", str.indexOf("Gaurav"));
