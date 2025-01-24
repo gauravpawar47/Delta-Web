fdb566796a6f3e315612ce1902ef1a4ec6d9bb9d
gauravpawar47 <gauravpawar883@gmail.com>
2025-01-24 19:01:54 +0530

    Chapter 16

---
+++ Chapter 16 - JavaScript (Part 3)/9_App.js
@@ -0,0 +1,16 @@
+// Task 1
+
+let msg = "help !";
+
+console.log("Message                 : ", msg);
+console.log("Message With Operations : ", msg.trim().toUpperCase());
+
+// Task 2 - output
+
+// Task 3
+
+let str = "ApnaCollege";
+
+console.log("String without Operation : ", str);
+console.log("String with Operation    : ", str.slice(4).replace('l', 't'));
+
