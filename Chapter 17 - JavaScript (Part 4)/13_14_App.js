b7e84076c21afac3793190b096bb4f0ebcf71f9f
gauravpawar47 <gauravpawar883@gmail.com>
2025-01-25 15:04:26 +0530

    Chapter 17

---
+++ Chapter 17 - JavaScript (Part 4)/13_14_App.js
@@ -0,0 +1,18 @@
+// 13. For Of Loops
+
+let fruits = ["Banana", "Mango", "Litchi", "Dragon", "Kiwi"];
+
+console.log("Fruits : ")
+for(fruit of fruits)
+{
+    console.log(fruit);
+}
+
+// 14. For Of (Nested)
+
+let names = [["Gaurav Pawar", "Nikam Pratik"], ["Kamlakskar Chinmay", "Kathe Gaurav"], ["Khan Faizan", "Shukla Chinmay"]];
+console.log("Names : ")
+for(name of names)
+{
+    console.log(name);
+}
