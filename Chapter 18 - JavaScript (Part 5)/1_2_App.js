e7b284ee684e5a227ab34c4c07e0ac262c674570
gauravpawar47 <gauravpawar883@gmail.com>
2025-01-26 15:13:10 +0530

    Chapter 18

---
+++ Chapter 18 - JavaScript (Part 5)/1_2_App.js
@@ -0,0 +1,8 @@
+const student = {
+    name : "Gaurav Pawar",
+    age : 20,
+    cgpa : 9.72,
+    dept : "COMPUTER ENGINEERING"
+};
+
+console.log(student);
---
+++ Chapter 18 - JavaScript (Part 5)/3_App.js
@@ -0,0 +1,9 @@
+const thread = {
+    username : "@student47",
+    content : "First Interview Podcast with Idols",
+    likes : 350,
+    repost : 10,
+    tag : ["@apnacollege", "@shraddhakhapra", "@amandhattarwal"],
+};
+
+console.log(thread);
