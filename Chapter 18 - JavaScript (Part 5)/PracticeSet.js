5cebad33fae2903f0cc2c1862796127eb7276b11
gauravpawar47 <gauravpawar883@gmail.com>
2025-01-26 16:42:30 +0530

    Chapter 18

---
+++ Chapter 18 - JavaScript (Part 5)/PracticeSet.js
@@ -0,0 +1,25 @@
+// Task 1
+
+let dice = Math.floor(Math.random() * 6) + 1;
+console.log("Random Number : ", dice)
+
+// Task 2
+
+const car = {
+    name : "Virtus",
+    model : "GT Plus Sport DSG",
+    color : "White"
+};
+
+console.log("Car : ", car);
+console.log("Car Name : ", car.name);
+
+// Task 3
+
+const person = {
+    name : "Gaurav Pawar",
+    age : 20,
+    city : "Nashik"
+};
+
+console.log(person);
