00b66c4a0a47c019f1e2d0055a0c404ac3a24366
gauravpawar47 <gauravpawar883@gmail.com>
2025-01-29 11:28:32 +0530

    Chapter 20

---
+++ Chapter 20 - JavaScript (Part 7)/1_App.js
@@ -0,0 +1,16 @@
+// 1. This Keyword
+
+const student ={
+    name: "Gaurav Pawar",
+    age: 20,
+    dept: "COMPUTER ENGINEERING",
+    year: "TE",
+    getDetails(){
+        console.log("Name : ", this.name);
+        console.log("Age  : ", this.age);
+        console.log("Year : ", this.dept);
+        console.log("Dept : ", this.year);
+    }
+}
+
+console.log(student.getDetails());
---
+++ Chapter 20 - JavaScript (Part 7)/2_App.js
@@ -0,0 +1,6 @@
+try{
+    console.log(a);
+}
+catch{
+    console.log("Variable Doesn't Exist");  
+}
