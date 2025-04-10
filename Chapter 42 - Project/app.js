7c9a9564606c472874d4525e471d3764f1b96a8d
gauravpawar47 <gauravpawar883@gmail.com>
2025-04-10 09:20:58 +0530

    Chapter 46

--- Chapter 42 - Project/app.js
+++ Chapter 42 - Project/app.js
@@ -6,6 +6,9 @@ const Listing = require("./models/listing");
 const path = require("path");
 const methodOverride = require("method-override");
 const ejsMate = require("ejs-mate");
+const wrapAsync = require("./utils/wrapAsync.js");
+const ExpressError = require("./utils/ExpressError.js");
+const { listingSchema } = require("./schema.js");
 
 const MONGO_URL = "mongodb://127.0.0.1:27017/wanderLust";
 
@@ -36,62 +39,100 @@ app.get("/listings/new", (req, res) => {
 });
 
 // Index Route - Show all listings
-app.get("/listings", async (req, res) => {
-  const allListings = await Listing.find({});
-  res.render("./listings/index.ejs", { allListings });
-});
+app.get(
+  "/listings",
+  wrapAsync(async (req, res) => {
+    const allListings = await Listing.find({});
+    res.render("./listings/index.ejs", { allListings });
+  })
+);
+
+// Validation for Schema (Middlewares)
+const validateListing = (req, res, next) => {
+  let { error } = listingSchema.validate(req.body);
+  if (error) {
+    let errMsg = error.details.map((el) => el.message).join(",");
+    throw new ExpressError(400, error);
+  } else {
+    next();
+  }
+};
 
 // Create Route - Add new listing
-app.post("/listings", async (req, res, next) => {
-  try {
+app.post(
+  "/listings",
+  validateListing,
+  wrapAsync(async (req, res, next) => {
+    let result = listingSchema.validate(req.body);
+    if (result.error) {
+      throw new ExpressError(400, result.error);
+    }
+
     const newListing = new Listing(req.body.listing);
     await newListing.save();
     res.redirect("/listings");
-  } catch (err) {
-    next(err);
-  }
-});
+  })
+);
 
 // Show Route - Show details of a listing
-app.get("/listings/:id", async (req, res) => {
-  let { id } = req.params;
-  const listing = await Listing.findById(id);
-  if (!listing) {
-    return res.status(404).send("Listing not found");
-  }
-  res.render("./listings/show.ejs", { listing });
-});
+app.get(
+  "/listings/:id",
+  wrapAsync(async (req, res) => {
+    let { id } = req.params;
+    const listing = await Listing.findById(id);
+    if (!listing) {
+      return res.status(404).send("Listing not found");
+    }
+    res.render("./listings/show.ejs", { listing });
+  })
+);
 
 // Edit Route - Show edit form
-app.get("/listings/:id/edit", async (req, res) => {
-  let { id } = req.params;
-  const listing = await Listing.findById(id);
-  if (!listing) {
-    return res.status(404).send("Listing not found");
-  }
-  res.render("./listings/edit.ejs", { listing });
-});
+app.get(
+  "/listings/:id/edit",
+  wrapAsync(async (req, res) => {
+    let { id } = req.params;
+    const listing = await Listing.findById(id);
+    if (!listing) {
+      return res.status(404).send("Listing not found");
+    }
+    res.render("./listings/edit.ejs", { listing });
+  })
+);
 
 // Update Route - Update listing details
-app.put("/listings/:id", async (req, res) => {
-  let { id } = req.params;
-  await Listing.findByIdAndUpdate(id, { ...req.body.listing });
-  res.redirect(`/listings/${id}`);
-});
+app.put(
+  "/listings/:id",
+  validateListing,
+  wrapAsync(async (req, res) => {
+    let { id } = req.params;
+    await Listing.findByIdAndUpdate(id, { ...req.body.listing });
+    res.redirect(`/listings/${id}`);
+  })
+);
 
 // Delete Route - Delete a listing
-app.delete("/listings/:id", async (req, res) => {
-  let { id } = req.params;
-  let deletedListing = await Listing.findByIdAndDelete(id);
-  if (!deletedListing) {
-    return res.status(404).send("Listing not found");
-  }
-  res.redirect("/listings");
+app.delete(
+  "/listings/:id",
+  wrapAsync(async (req, res) => {
+    let { id } = req.params;
+    let deletedListing = await Listing.findByIdAndDelete(id);
+    if (!deletedListing) {
+      return res.status(404).send("Listing not found");
+    }
+    res.redirect("/listings");
+  })
+);
+
+// Adding Express Error
+app.all("*", (req, res, next) => {
+  next(new ExpressError(404, "Page Not Found !"));
 });
 
 // Error Handling Middleware
 app.use((err, req, res, next) => {
-  res.send("Something Went Wrong !");
+  let { statusCode = 500, message = "Something Went Wrong" } = err;
+  res.status(statusCode).render("error.ejs", { err });
 });
 
 // Start Server
