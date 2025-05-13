881027db95724bc50bba166c048d575b32d57129
gauravpawar47 <gauravpawar883@gmail.com>
2025-05-13 12:23:50 +0530

    Chapter 58

--- Chapter 57 - React (Part 1)/Chapter 57 (Basic React App)/src/MainActivitiy.jsx
+++ Chapter 57 - React (Part 1)/Chapter 57 (Basic React App)/src/MainActivitiy.jsx
@@ -5,7 +5,7 @@ export default function MainActivity({ title, description, price }) {
     <div className="card">
       <h2>{title}</h2>
       <h4>{description}</h4>
-      <h3 className="price">
+      <h3>
         <span className="old-price">₹{price.old}</span> →
         <span className="new-price"> ₹{price.new}</span>
       </h3>
