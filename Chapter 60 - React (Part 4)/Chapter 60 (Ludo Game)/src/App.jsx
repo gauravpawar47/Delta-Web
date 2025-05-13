c70787c85aabf2112f6349c8a61efedbdeb13167
gauravpawar47 <gauravpawar883@gmail.com>
2025-05-13 23:43:37 +0530

    Chapter 60

---
+++ Chapter 60 - React (Part 4)/Chapter 60 (Ludo Game)/src/App.jsx
@@ -0,0 +1,13 @@
+import { useState } from "react";
+import "./App.css";
+import LudoGame from "./LudoGame";
+
+function App() {
+  return (
+    <>
+    <LudoGame/>
+    </>
+  );
+}
+
+export default App;
---
+++ Chapter 60 - React (Part 4)/Chapter 60 (Ludo Game)/src/LudoGame.jsx
@@ -0,0 +1,51 @@
+import { useState } from "react";
+
+export default function LudoGame() {
+  const [moves, setMoves] = useState({ blue: 0, red: 0, yellow: 0, green: 0 });
+
+  const updateMove = (color) => {
+    setMoves((prevMoves) => ({
+      ...prevMoves,
+      [color]: prevMoves[color] + 1,
+    }));
+  };
+
+  return (
+    <div>
+      <p>Game Begins</p>
+      <div className="board">
+        <p>Blue Moves : {moves.blue}</p>
+        <button
+          style={{ backgroundColor: "blue", color: "black" }}
+          onClick={() => updateMove("blue")}
+        >
+          +1
+        </button>
+
+        <p>Yellow Moves : {moves.yellow}</p>
+        <button
+          style={{ backgroundColor: "yellow", color: "black" }}
+          onClick={() => updateMove("yellow")}
+        >
+          +1
+        </button>
+
+        <p>Green Moves : {moves.green}</p>
+        <button
+          style={{ backgroundColor: "green", color: "black" }}
+          onClick={() => updateMove("green")}
+        >
+          +1
+        </button>
+
+        <p>Red Moves : {moves.red}</p>
+        <button
+          style={{ backgroundColor: "red", color: "black" }}
+          onClick={() => updateMove("red")}
+        >
+          +1
+        </button>
+      </div>
+    </div>
+  );
+}
