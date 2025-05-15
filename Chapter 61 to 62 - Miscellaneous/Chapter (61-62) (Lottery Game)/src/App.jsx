2dd08bf4e8fca95e631db52116822b4b98e9c28c
gauravpawar47 <gauravpawar883@gmail.com>
2025-05-15 09:31:55 +0530

    Chapter 62

--- Chapter 61 to 62 - Miscellaneous/Chapter (61-62) (Lottery Game)/src/App.jsx
+++ Chapter 61 to 62 - Miscellaneous/Chapter (61-62) (Lottery Game)/src/App.jsx
@@ -2,9 +2,13 @@ import "./App.css";
 import Lottery from "./Lottery";
 
 function App() {
+  let winCondition = (ticket) => {
+    return ticket[0] === 0;
+  };
+
   return (
     <>
-      <Lottery />
+      <Lottery n={3} winCondition={winCondition} />
     </>
   );
 }
--- Chapter 61 to 62 - Miscellaneous/Chapter (61-62) (Lottery Game)/src/Lottery.jsx
+++ Chapter 61 to 62 - Miscellaneous/Chapter (61-62) (Lottery Game)/src/Lottery.jsx
@@ -1,25 +1,21 @@
 import { use, useState } from "react";
 import "./Lottery.css";
 import { genTicket, sum } from "./Helper";
+import Ticket from "./Ticket";
 
-export default function Lottery() {
+export default function Lottery({ n = 3, winCondition }) {
   let [ticket, setTicket] = useState(genTicket(3));
-  let isWinning = sum(ticket) === 15;
+  let isWinning = winCondition(ticket);
 
   let buyTicket = () => {
-    setTicket(genTicket(3));
+    setTicket(genTicket(n));
   };
 
   return (
     <div>
       <h1>Lottery Game !</h1>
-      <div className="ticket">
-        <span>{ticket[0]}</span>
-        <span>{ticket[1]}</span>
-        <span>{ticket[2]}</span>
-      </div><br />
+      <Ticket ticket={ticket} />
       <button onClick={buyTicket}>Buy Ticket</button>
-
       <h3>{isWinning && "Congratulations You Won !!"}</h3>
     </div>
   );
